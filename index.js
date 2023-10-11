ba27f1bb7fdccceb0a8473a7dd4756d86360434c
John Komba <john.mwangi1@student.moringaschool.com>
2023-10-11 12:46:07 +0300

    fetching data from my API

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ document.addEventListener('DOMContentLoaded', () => {
 function searchMovies(query) {
     const movieList = document.getElementById('movieList');
     movieList.innerHTML = '';
+    fetch(`http://www.omdbapi.com/?s=${query}&apikey=YOUR_API_KEY`)
+    .then(response => response.json())
+    .then(data => {
+        if (data.Response === 'True') {
+            data.Search.forEach(movie => {
+                const listItem = document.createElement('li');
+                listItem.textContent = `${movie.Title} (${movie.Year})`;
+                movieList.appendChild(listItem);
+            });
+        } else {
+            const listItem = document.createElement('li');
+            listItem.textContent = 'No movies found';
+            movieList.appendChild(listItem);
+        }
