a54db08ffea0672ff34359154ae18d113022d5ad
John Komba <john.mwangi1@student.moringaschool.com>
2023-10-12 10:08:10 +0300

    adding some interactivity to my web app

--- index.js
+++ index.js
@@ -5,7 +5,22 @@ document.addEventListener('DOMContentLoaded', () => {
         const searchInput = document.getElementById('searchInput').value;
         searchMovies(searchInput);
     });
+
+    const likeButton = document.getElementById('likeButton');
+
+    likeButton.addEventListener('click', () => {
+        const likeCount = document.getElementById('likeCount');
+        likeCount.textContent = parseInt(likeCount.textContent) + 1;
+    });
+
+    const checkbox = document.getElementById('subscribeCheckbox');
+
+    checkbox.addEventListener('change', () => {
+        const subscriptionStatus = document.getElementById('subscriptionStatus');
+        subscriptionStatus.textContent = checkbox.checked ? 'Subscribed' : 'Unsubscribed';
+    });
 });
+
 function searchMovies(query) {
     const movieList = document.getElementById('movieList');
     movieList.innerHTML = '';
@@ -16,11 +31,21 @@ function searchMovies(query) {
     .then(data => {
         if (data.Response === 'True') {
             data.Search.forEach(movie => {
-                const listItem = document.createElement('li');
-                listItem.textContent = `${movie.Title} (${movie.Year})`;
-                movieList.appendChild(listItem);
+                // Fetch additional details about the movie using its imdbID
+                fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=${apiKey}`)
+                    .then(response => response.json())
+                    .then(movieDetails => {
+                        const listItem = document.createElement('li');
+                        listItem.innerHTML = `
+                            <h3>${movieDetails.Title} (${movieDetails.Year})</h3>
+                            <p>Director: ${movieDetails.Director}</p>
+                            <p>Genre: ${movieDetails.Genre}</p>
+                            <p>Plot: ${movieDetails.Plot}</p>
+                            <img src="${movieDetails.Poster}" alt="${movieDetails.Title}">
+                        `;
+                        movieList.appendChild(listItem);
+                    });
             });
-            
         } else {
             const listItem = document.createElement('li');
             listItem.textContent = 'No movies found';
